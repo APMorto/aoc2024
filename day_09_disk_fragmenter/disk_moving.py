06f98dca1f405ce3d9a096476c061bd022919846
Andrew <apm946@usask.ca>
2024-12-09 09:55:54 -0600

    Cleaned up day 9 part 2

--- day_09_disk_fragmenter/disk_moving.py
+++ day_09_disk_fragmenter/disk_moving.py
@@ -66,15 +66,14 @@ def part1(line: str):
 # and blocks at some positions
 # and we attempt moving in decreasing id
 def part2(line: str):
-    s_len = len(line)
     nums = list(map(int, line))
 
+    # A quickly insertible and min-queriable list of positions for each free size
     available_blocks_of_size = [SortedList() for _ in range(10)]  # [0] == [] should always hold
     initial_block_positions = []    # (id, pos, size)
 
+    # Initialize available blocks and initial positions
     j = 0
-    #for i in range(s_len-1, -1, -1):
-    #    num = nums[i]
     for i, num in enumerate(nums):
         if i % 2 == 0:
             initial_block_positions.append((i // 2, j, num))
@@ -86,49 +85,33 @@ def part2(line: str):
     out = 0
     for id, pos, size in reversed(initial_block_positions):
         # Dest is the smallest value, not the smallest value of the smallest space.
+        # Find where we will be putting the file.
         dest = pos
         chosen_size = -1
         for candidate_size in range(size, 10):
             sl = available_blocks_of_size[candidate_size]
             if len(sl) == 0:
                 continue
-            #dest = min(dest, sl[0])
             if sl[0] < dest:
                 chosen_size = candidate_size
                 dest = sl[0]
 
+        # Move the file if we can
         if dest < pos:
-        #for candidate_size in range(size, 10):
-        #    sl = available_blocks_of_size[candidate_size]
-        #    if len(sl) == 0:
-        #        continue
-        #    # We can put it somwhere!
-        #    dest = sl.pop(0)
             available_blocks_of_size[chosen_size].pop(0)
-            #print(f"Putting {(id, pos, size)} at {dest}")
 
-            # Put the remaining space back in
+            # Put the remaining space back in. This is why we have the fancy ordered lists.
             remaining_size = chosen_size - size
             if remaining_size > 0:
                 available_blocks_of_size[remaining_size].add(dest + size)
 
-            # Free up where we were
-            available_blocks_of_size[size].add(pos)
+            # We do NOT need to free up where we were, as that's right of what matters.
 
-            # Add the results from this
-            for k in range(dest, dest+size):
-                out += k * id
-
-        else:
-            #print(f"Could not put {(id, pos, size)} anywhere. leaving")
-            # We cannot put it anywhere.
-            # Just add the results from this
-            for k in range(pos, pos+size):
-                out += k * id
+        # Add the results from this.
+        for k in range(dest, dest+size):
+            out += k * id
 
     return out
-
-
 
 
 if __name__ == '__main__':
