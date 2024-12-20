292ae33a506a5c841327945aef9740bb5135399a
Andrew <apm946@usask.ca>
2024-12-20 11:42:04 -0600

    Started attempting day 17 part 2. Brute force not enough. Seen set is too much memory.

--- day_17_chronospatial_computer/chronospatial_computer.py
+++ day_17_chronospatial_computer/chronospatial_computer.py
@@ -33,6 +33,13 @@ class ChronospatialComputer:
         self.halted: bool = False
         self.n: int = len(program)
         self.output_list = []
+        self.expected_output = None
+        self.expects = False
+        self.valid = True
+
+    def expect(self, expected):
+        self.expected_output = expected
+        self.expects = True
 
     def run_to_halting(self):
         while not self.execute():
@@ -63,6 +70,12 @@ class ChronospatialComputer:
                 self.B ^= self.C
             case 5: # 0b101
                 self.output_list.append(combo_operand & 0b111)
+                if self.expects:
+                    l = len(self.output_list)
+                    if l > len(self.output_list):
+                        self.valid = False
+                    elif self.output_list[l - 1] != self.expected_output[l-1]:
+                        self.valid = False
                 #self.outputs(combo_operand & 0b111)
             case 6: # 0b110
                 self.B = math.trunc(self.A / (1 << combo_operand))
@@ -70,13 +83,16 @@ class ChronospatialComputer:
                 self.C = math.trunc(self.A / (1 << combo_operand))
 
         self.IP += 2
+        return False
+
+    def state(self):
+        return self.A, self.B, self.C, self.IP, len(self.output_list)
 
     def outputs(self, val: int):
         self.output_list.append(val)
 
     def __repr__(self):
         return f"A: {self.A}, B: {self.B}, C: {self.C}, IP: {self.IP}, OUT: {self.output_list}"
-
 
 
 def initialalize_computer(line_blocks):
@@ -88,15 +104,42 @@ def initialalize_computer(line_blocks):
     return ChronospatialComputer(program, A, B, C)
 
 
-
 def part1(line_blocks):
     computer = initialalize_computer(line_blocks)
     computer.run_to_halting()
     return ",".join(map(str, computer.output_list))
 
 
-def part2(_):
-    pass
+def part2(line_blocks):
+    registers, program_string = line_blocks
+    A = int(registers[0][len("Register _: "):])
+    B = int(registers[1][len("Register _: "):])
+    C = int(registers[2][len("Register _: "):])
+    program = list(map(int, program_string[0][len("Program: "):].split(",")))
+
+    seen_states = set()
+
+    # For what value of A will this computer output itself?
+    for A in range(0, 1 << 32):
+        if A & 1 << 30:
+            print("Iteration:", A)
+
+        computer = ChronospatialComputer(program, A, B, C)
+        computer.expect(program)
+
+        while computer.valid:
+            if computer.execute():
+                assert computer.halted
+                break
+            state = computer.state()
+            if state in seen_states:
+                break
+            seen_states.add(state)
+        if computer.valid and len(computer.output_list) == len(program):
+            return A
+
+    return None
+
 
 
 # not 2,3,1,2,4,3,5,2,2
@@ -106,5 +149,5 @@ if __name__ == '__main__':
     get_results("P1 Example", part1, read_line_blocks, "example.txt", expected="4,6,3,5,6,3,5,2,1,0")
     get_results("P1", part1, read_line_blocks, "input.txt")
 
-    get_results("P2 Example", part2, read_line_blocks, "example.txt")
+    get_results("P2 Example", part2, read_line_blocks, "example2.txt", expected=117440)
     get_results("P2", part2, read_line_blocks, "input.txt")
