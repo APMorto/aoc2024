186fa352bda6184ec960c0b05bee0bc4f444257f
Andrew <apm946@usask.ca>
2024-12-19 22:15:33 -0600

    Did day 19 part 2

--- day_19_linen_layout/linen_layout.py
+++ day_19_linen_layout/linen_layout.py
@@ -28,14 +28,34 @@ def part1(blocks: List[str]):
     return out
 
 
+def num_ways_to_make(pattern, pattern_set, lengths):
+    n = len(pattern)
+    dp = [0] * (n+1)
+    dp[0] = 1
+    for i in range(1, n+1):
+        for l in lengths:
+            if l > i:
+                break
+            if dp[i-l] and pattern[i-l: i] in pattern_set:
+                dp[i] += dp[i-l]
 
-def part2(_):
-    pass
+    return dp[n]
+
+
+
+def part2(blocks):
+    pattern_set = set(blocks[0][0].split(", "))
+    pattern_lengths = sorted(set(len(p) for p in pattern_set))
+
+    out = 0
+    for pattern in blocks[1]:
+        out += num_ways_to_make(pattern, pattern_set, pattern_lengths)
+    return out
 
 
 if __name__ == '__main__':
-    get_results("P1 Example", part1, read_line_blocks, "example.txt")
+    get_results("P1 Example", part1, read_line_blocks, "example.txt", expected=6)
     get_results("P1", part1, read_line_blocks, "input.txt")
 
-    get_results("P2 Example", part2, read_line_blocks, "example.txt")
+    get_results("P2 Example", part2, read_line_blocks, "example.txt", expected=16)
     get_results("P2", part2, read_line_blocks, "input.txt")
