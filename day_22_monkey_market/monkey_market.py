897f40a66e15b9562bb178db237a34fd9fb2d345
Andrew <apm946@usask.ca>
2024-12-22 15:23:20 -0600

    Formatted file to be readable (somewhat)

--- day_22_monkey_market/monkey_market.py
+++ day_22_monkey_market/monkey_market.py
@@ -9,13 +9,43 @@ from typing import List
 from util.matrix_math import row_vector_of_integer, integer_of_row_vector, left_shift_of_matrix, \
     right_shift_of_matrix, matrix_power_linear, matrix_power_log
 
-MOD = 16777216 # 1 << 24
-#PRUNE_MASK = (1 << 24) - 1  # 0b[1]^24
-P1_ITERS = 2000
+# The process of cycling the secret number / hashing represents a linear operation, and thus can be represented as a matrix.
+#   (Where entries are binary / integers modulo 2)
+#   (XOR is equivalent to addition in integers modulo 2)
+# Thus the two-thousandth number is given by INITIAL * M^2000, where M is our secret number cycling matrix.
+# I have precomputed M^2000, and have implemented 'fast' bitwise operations to apply the matrix.
 
+def part1_binary_matrix_operations(lines: List[str]):
+    out = 0
+    for line in lines:
+        num = int(line)
+        out += raise_to_2k_power(num)
+    return out
+
+# This is just [ROW VECTOR] * [MATRIX], but entries are in integers modulo 2, and we've fixed the matrix.
+def raise_to_2k_power(num):
+    out = 0
+    for i, col_val in enumerate(COLUMN_VALUES):
+        bin_val = num & col_val         # dot product.
+        bin_val ^= bin_val >> 16        # Sum of values % 2
+        bin_val ^= bin_val >> 8         # Repeated addition mod 2 in accumulators of the least significant bit portions.
+        bin_val ^= bin_val >> 4         # Eg 0b1010|1001 -> 0bXXXX0011, where | denotes the separation,
+        bin_val ^= bin_val >> 2         # and 0, 0, 1, 1 in the result are all accumulators representing 1+1, 0+0, 0+1, 1+0, all % 2
+        bin_val ^= bin_val >> 1         # Values to the left of our accumulator are junk.
+        out += (bin_val & 1) << i       # Place this result in the ith row (bit)
+    return out
+
+# Old part 1.
+def part1(lines: List[str]):
+    out = 0
+    for line in lines:
+        num = int(line)
+        two_thousands_secret = kth_secret_number(num, 2000)
+        #print(num, two_thousands_secret)
+        out += two_thousands_secret
+    return out
 
 #cur = (cur ^ (cur << 6) ^ (cur >> 5) ^ (cur << 11)) & PRUNE_MASK  # is WRONG! Because we need to update cur between uses.
-
 def kth_secret_number(initial_secret_number: int, k: int) -> int:
     PRUNE_MASK = (1 << 24) - 1  # 0b[1]^24
     cur = initial_secret_number
@@ -25,38 +55,12 @@ def kth_secret_number(initial_secret_number: int, k: int) -> int:
         cur = (cur ^ (cur << 11)) & PRUNE_MASK
     return cur
 
-def part1(lines: List[str]):
-    out = 0
-    for line in lines:
-        num = int(line)
-        two_thousands_secret = kth_secret_number(num, P1_ITERS)
-        #print(num, two_thousands_secret)
-        out += two_thousands_secret
-    return out
 
-
-# If we start part way into the sequence, we don't take it.
-# We get to choose ONE sequence of four price changes.
-# When the monkey sees that sequence (in its entirety), we buy the banana!
-# We wish to maximize bananas.
-# Simply store all sequences we've seen, packed into some integer.
-# Then take the max.
-
-def make_sequence_integer(*args):
-    sequence_integer = 0
-    CHANGE_WIDTH = 5
-    SEQUENCE_MASK = (1 << (CHANGE_WIDTH * 4)) - 1
-    for change in args:
-        sequence_integer = ((sequence_integer << CHANGE_WIDTH) | (change+10)) & SEQUENCE_MASK
-    return sequence_integer
-
-
+# Nothing special for part 2, as we actually need to see the intermediate values.
 def part2(lines: List[str]):
     PRUNE_MASK = (1 << 24) - 1  # 0b[1]^24
     CHANGE_WIDTH = 5
     SEQUENCE_MASK = (1 << (CHANGE_WIDTH * 4)) - 1
-    #SECOND_SHIFT_MASK = ~((1<<23) | (1<<22) | (1<<21) | (1<<20) | (1<<19))  # 5 leftmost bits are to be gone
-    #THIRD_SHIFT_MASK =
     counts = defaultdict(int)
     for line in lines:
         already_sold = set()
@@ -100,6 +104,22 @@ def part2(lines: List[str]):
 # Assuming that you can do that
 
 
+# If we start part way into the sequence, we don't take it.
+# We get to choose ONE sequence of four price changes.
+# When the monkey sees that sequence (in its entirety), we buy the banana!
+# We wish to maximize bananas.
+# Simply store all sequences we've seen, packed into some integer.
+# Then take the max.
+
+def make_sequence_integer(*args):
+    sequence_integer = 0
+    CHANGE_WIDTH = 5
+    SEQUENCE_MASK = (1 << (CHANGE_WIDTH * 4)) - 1
+    for change in args:
+        sequence_integer = ((sequence_integer << CHANGE_WIDTH) | (change+10)) & SEQUENCE_MASK
+    return sequence_integer
+
+
 def get_hash_matrix():
     # This matrix represents Taking a row vector of the bits, and applying this will yield the next hash state.
     cur = np.eye(24, 24, dtype=np.uint8)
@@ -110,13 +130,6 @@ def get_hash_matrix():
     left_shift_11 = left_shift_of_matrix(cur, 11)
     cur ^= left_shift_11
     return cur
-
-def part1_binary_matrix_operations(lines: List[str]):
-    out = 0
-    for line in lines:
-        num = int(line)
-        out += raise_to_2k_power(num)
-    return out
 
 def part1_matrix(lines: List[str]):
     #hash_matrix = get_hash_matrix()
@@ -173,22 +186,7 @@ TWO_THOUSANDTH_EXPONENTIATED_MATRIX = np.array([
 # values of the columns, going up vertically.
 COLUMN_VALUES = [11593647, 115592, 9650831, 11935952, 464073, 1874787, 8943425, 6826032, 5526809, 5714711, 9844545, 10663580, 9219426, 7597282, 491792, 9818029, 7423510, 8679750, 6442813, 10841274, 900302, 10889424, 14239094, 2959297]
 
-def raise_to_2k_power(num):
-    out = 0
-    for i, col_val in enumerate(COLUMN_VALUES):
-        bin_val = num & col_val # dot product.
 
-        # Sum of values % 2
-        # Repeated addition mod 2 in accumulators of the least significant bit portions.
-        # Eg 0b1010|1001 -> 0bXXXX0011, where | denotes the separation,
-        # and 0, 0, 1, 1 in the result are all accumulators representing 1+1, 0+0, 0+1, 1+0, all % 2
-        bin_val ^= bin_val >> 16
-        bin_val ^= bin_val >> 8
-        bin_val ^= bin_val >> 4
-        bin_val ^= bin_val >> 2
-        bin_val ^= bin_val >> 1
-        out += (bin_val & 1) << i   # Values to the left of our accumulator are junk.
-    return out
 
 
 
