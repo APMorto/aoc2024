a1664a873d797a8a06227f62a05b01845e35cc7a
Andrew <apm946@usask.ca>
2024-12-25 13:28:32 -0600

    Started speeding up day 2.

--- day_02_red_nosed_reports/red_nosed_reports.py
+++ day_02_red_nosed_reports/red_nosed_reports.py
@@ -43,6 +43,79 @@ def part2(lines: List[str]):
 
     return out
 
+def both_parts(lines: List[str]):
+    out1 = 0
+    out2 = 0
+    for line in lines:
+        numbers = list(map(int, line.split()))
+        n = len(numbers)
+
+        # Its Possible that we may remove some of the first numbers.
+        up = 0
+        down = 0
+        # Look at the first 3 deltas to determine the monotonicity (len(numbers)) >= 4
+        for i in range(4):
+            if numbers[i] < numbers[i + 1]:
+                up += 1
+            #elif numbers[i] > numbers[i + 1]:
+            else:
+                down += 1
+
+        bad_connection1 = -2
+        bad_connection2 = -2
+        broken = False
+        if up > down:
+            # Monotonically increasing.
+            for i in range(1, n):
+                if numbers[i-1] >= numbers[i] or numbers[i] - numbers[i-1] > 3:
+                    if bad_connection1 == -2:
+                        bad_connection1 = i
+                    else:
+                        bad_connection2 = i
+                        if bad_connection1 != i-1:
+                            broken = True
+                            break
+
+
+            if broken:
+                continue
+
+            if bad_connection1 == -2:   # No bad connection
+                out1 += 1
+                out2 += 1
+            elif bad_connection1 == bad_connection2 - 1:    # 2 bad connections
+                # Is it fixed by removing value on bad connection 1?
+                if numbers[bad_connection1-1] < numbers[bad_connection2] and numbers[bad_connection2] - numbers[bad_connection1-1] <= 3:
+                    out2 += 1
+            else: # Remove either bad_connection1 or bad_connection1-1
+                if (bad_connection1 < n-1 and numbers[bad_connection1-1] < numbers[bad_connection1+1] and numbers[bad_connection1+1] - numbers[bad_connection1-1] <= 3) or \
+                    (bad_connection1 > 1 and  numbers[bad_connection1-2] < numbers[bad_connection1]   and numbers[bad_connection1] -   numbers[bad_connection1-2] <= 3) or \
+                        bad_connection1 == 1 or bad_connection1 == n-1:
+                    out2 += 1
+
+        else:
+            # Use old part 1
+            if is_safe(numbers):
+                out1 += 1
+                out2 += 1
+            else:
+                n = len(numbers)
+                hold = numbers.pop(n - 1)
+                if is_safe(numbers):
+                    out2 += 1
+                else:
+                    for i in range(n - 2, -1, -1):
+                        hold, numbers[i] = numbers[i], hold
+                        if is_safe(numbers):
+                            out2 += 1
+                            break
+
+
+
+
+
+
+    return out1, out2
 
 if __name__ == '__main__':
     get_results("P1 Example", part1, read_lines, "example.txt", expected=2)
@@ -50,3 +123,4 @@ if __name__ == '__main__':
 
     get_results("P2 Example", part2, read_lines, "example.txt", expected=4)
     get_results("P2", part2, read_lines, "input.txt")
+    get_results("P2 Faster", both_parts, read_lines, "input.txt")
